8785a6ea8e499bf5ddff854e688ac6c289d4b189
Taylor Brown <t@ylorbrown.com>
2013-10-21 16:36:35 -0500

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+	# Returns the Gravatar for given user.
+	def gravatar_for(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "//secure.gravatar.com/avatar/#{gravatar_id}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
 end
